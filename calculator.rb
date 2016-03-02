09742c1b7ba9dcec76388d9849083f519a4c2327
michaelscotthurley <michaelscotthurley>
2016-03-01 22:29:14 -0800

    Calculator complete

---
+++ calculator.rb
@@ -0,0 +1,30 @@
+def calculator(calc, num1, num2)
+  if calc.downcase == 'add'
+    result = num1 + num2
+  elsif calc.downcase == 'sub'
+    result = num1 - num2
+  elsif calc.downcase == 'mult'
+    result = num1 * num2
+  elsif calc.downcase == 'div'
+    result = num1/num2
+  else
+    puts 'Invalid input'
+  end
+  puts 'The answer is: '
+  puts result
+end
+
+puts 'What calculation would you like to do? Please choose "add", "sub", "mult" or "div".'
+calc = gets.chomp
+puts "Please enter your first number."
+num1 = gets.chomp.to_i
+puts "Please enter your second number."
+num2 = gets.chomp.to_i
+
+calculator(calc, num1, num2) 
+
+
+
+  
+
+
